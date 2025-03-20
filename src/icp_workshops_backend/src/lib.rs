dc86ffb46de07f13ffcea7a1bba39ac6ea788882
Patryczek 1 <patryksuchy1@interia.pl>
2025-03-20 11:21:55 +0000

    yo

--- src/icp_workshops_backend/src/lib.rs
+++ src/icp_workshops_backend/src/lib.rs
@@ -2,3 +2,21 @@
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+
+#[ic_cdk::query]
+fn calculate(a: i32, b: i32, operator: String)  -> i32 {
+    let result = match operator.as_str() {
+        "+" => Some(a+b),
+        "-" => Some(a-b),
+        "*" => Some(a*b),
+        "/" => b !== 0 {Some(a/b)} else {None},
+        _ => None
+    }
+
+    match {
+        Some(val) => format!("Result {}", val),
+        None => "Invalid operator or division by zero".to_string();
+    }
+
+}
